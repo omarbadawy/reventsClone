1ba6a824f88a3cc8b4e0bf16bf69b77faf19852b
omar badawy <omarbadawy10022@gmail.com>
2020-03-31 06:03:20 +0200

    end of routing

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink, Link, withRouter } from 'react-router-dom';
+import SignedOutMenu from '../Menus/SignedOutMenu';
+import SignedInMenu from '../Menus/SignedInMenu';
 
 class NavBar extends Component {
+    state = {
+        authenticated: false
+    };
+
+    handleSignIn = () => this.setState({ authenticated: true });
+    handleSignOut = () => {
+        this.setState({ authenticated: false });
+        this.props.history.push('/');
+    };
+
     render() {
+        const { authenticated } = this.state;
+
         return (
             <Menu inverted fixed="top">
                 <Container>
                     <Menu.Item as={NavLink} exact to="/" header>
-                        <img src="./assets/logo.png" alt="logo" />
+                        <img src="/assets/logo.png" alt="logo" />
                         Re-vents
                     </Menu.Item>
                     <Menu.Item as={NavLink} to="/events" name="Events" />
@@ -23,14 +37,15 @@ class NavBar extends Component {
                             content="Create Event"
                         />
                     </Menu.Item>
-                    <Menu.Item position="right">
-                        <Button basic inverted content="Login" />
-                        <Button basic inverted content="Sign Out" style={{ marginLeft: '0.5em' }} />
-                    </Menu.Item>
+                    {authenticated ? (
+                        <SignedInMenu signOut={this.handleSignOut} />
+                    ) : (
+                        <SignedOutMenu signIn={this.handleSignIn} />
+                    )}
                 </Container>
             </Menu>
         );
     }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
