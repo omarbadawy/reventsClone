da6e51d1c220b06a38a5e73d2e6f1ec4d47c7a0e
omar badawy <omarbadawy10022@gmail.com>
2020-04-01 23:50:44 +0200

    basic CRUD functionality with redux added

---
+++ src/app/store/configureStore.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux';
+import rootReducer from '../reducers/rootReducer';
+
+export const configureStore = () => {
+    const store = createStore(rootReducer);
+
+    return store;
+};
--- src/index.js
+++ src/index.js
@@ -4,23 +4,19 @@ import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from './app/store/configureStore';
+
+const store = configureStore();
 
 const rootEl = document.getElementById('root');
 
-// let render = () => {
-//     ReactDOM.render(<App />, rootEl);
-// };
-
-// if (module.hot) {
-//     module.hot.accept('./app/layout/App', () => {
-//         setTimeout(render);
-//     });
-// }
-
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     rootEl
 );
 
